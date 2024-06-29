99483fb37e5b1f0fd2e3a38ae004c8c803e81f2a
Asgar_George <asgarantony@gmail.com>
2024-06-29 20:24:32 +0530

    working with the snake Class

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    private int Row;
+    private int Col;
+    public Node(int row, int col) {
+        Row = row;
+        Col = col;
+    }
+
+    public int getRow() {
+        return Row;
+    }
+
+    public void setRow(int row) {
+        Row = row;
+    }
+
+    public int getCol() {
+        return Col;
+    }
+
+    public void setCol(int col) {
+        Col = col;
+    }
 }
